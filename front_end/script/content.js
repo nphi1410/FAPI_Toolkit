92a788aa89e6914df92c834b624e7e40731a9a5c
i-am-truong <tqtolympia@gmail.com>
2024-03-11 22:54:56 +0700

    change content.js

--- front_end/script/content.js
+++ front_end/script/content.js
@@ -296,19 +296,6 @@ header.innerHTML = `
   </div>
 `
 
-var items = document.querySelectorAll("[data-i18n]");
-var i;
-var l = items.length;
-for(i = 0; i < l; i++){
-	var translation = chrome.i18n.getMessage(items[i].getAttribute("data-i18n"));
-	if(items[i].value === "i18n"){
-		items[i].value = translation;
-	}else{
-		items[i].innerText = translation;
-	}
-}
-
-
 var setting = document.querySelector('.header-settings');
 var settingMenu = setting.querySelector('.header-settings-menu');
 setting.onclick = () => {
