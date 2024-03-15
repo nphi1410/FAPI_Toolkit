9b02f8c54e394b710859204e9711c207f657200f
DuongMinhSon16012004 <sondmhe186291@fpt.edu.vn>
2024-03-15 23:27:03 +0700

    fix feedback

--- front_end/FeedbackTeacher/feedback-teach.js
+++ front_end/FeedbackTeacher/feedback-teach.js
@@ -1,17 +1,17 @@
-var elements = document.querySelectorAll(
-    '[id^="ctl00_mainContent_reload_ctl"][id$="_chkList_0"]'
-);
-console.log(elements);
-
-for (e of elements) {
-    e.checked = true
+var success = document.querySelector('#ctl00_mainContent_lblMege');
+if(success.textContent === ""){
+        var elements = document.querySelectorAll(
+            '[id^="ctl00_mainContent_reload_ctl"][id$="_chkList_0"]'
+        );
+        console.log(elements);
+        
+        for (e of elements) {
+            e.checked = true
+        }
+        
+        var submit = document.querySelector(
+            '#ctl00_mainContent_btSendFeedback'
+        );
+        console.log(submit);
+        submit.click();
 }
-// for of: tra ra cac nodelist
-
-var submit = document.querySelector(
-    '#ctl00_mainContent_btSendFeedback'
-);
-console.log(submit);
-submit.click();
-
-//ádasdasdasd
