6eba1b89dfe71c8812aede7e9553a7a436ac9f0b
DuongMinhSon16012004 <sondmhe186291@fpt.edu.vn>
2024-03-18 20:15:01 +0700

    navbar

--- front_end/Header&Navbar/content.js
+++ front_end/Header&Navbar/content.js
@@ -84,15 +84,7 @@ contentBody.outerHTML =
             <!-- Dropdown menu -->
             <div id="dropdownNavbar" class="z-10 absolute hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
                 <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownLargeButton">
-                  <li>
-                    <a href="${listInfoLink[0]}" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">${listInfoText[0]}</a>
-                  </li>
-                  <li>
-                    <a href="${listInfoLink[1]}" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">${listInfoText[1]}</a>
-                  </li>
-                  <li>
-                    <a href="${listInfoLink[2]}" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">${listInfoText[2]}</a>
-                  </li>
+                  
                 </ul>
             </div>
         </li>
@@ -109,13 +101,13 @@ contentBody.outerHTML =
             <div id="dropdownNavbar" class="z-10 absolute hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
                 <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownLargeButton">
                   <li>
-                    <a href="${listInfoLink[0]}" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">${listInfoText[0]}</a>
+                    <a href="${listInfoLink[0]}" class="block px-4 hover:no-underline py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">${listInfoText[0]}</a>
                   </li>
                   <li>
-                    <a href="${listInfoLink[1]}" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">${listInfoText[1]}</a>
+                    <a href="${listInfoLink[1]}" class="block px-4 hover:no-underline py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">${listInfoText[1]}</a>
                   </li>
                   <li>
-                    <a href="${listInfoLink[2]}" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">${listInfoText[2]}</a>
+                    <a href="${listInfoLink[2]}" class="block px-4 hover:no-underline py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">${listInfoText[2]}</a>
                   </li>
                 </ul>
             </div>
@@ -133,75 +125,41 @@ contentBody.outerHTML =
             <div id="dropdownNavbar" class="z-10 absolute hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
                 <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownLargeButton">
                   <li>
-                    <a href="${listReportLink[0]}" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">${listReportText[0]}</a>
+                    <a href="${listReportLink[0]}" class="block px-4 hover:no-underline py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">${listReportText[0]}</a>
                   </li>
                   <li>
-                    <a href="${listReportLink[1]}" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">${listReportText[1]}</a>
+                    <a href="${listReportLink[1]}" class="block px-4 hover:no-underline py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">${listReportText[1]}</a>
                   </li>
                   <li>
-                    <a href="${listReportLink[2]}" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">${listReportText[2]}</a>
+                    <a href="${listReportLink[2]}" class="block px-4 hover:no-underline py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">${listReportText[2]}</a>
                   </li>
                   <li>
-                    <a href="${listReportLink[3]}" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">${listReportText[3]}</a>
+                    <a href="${listReportLink[3]}" class="block px-4 hover:no-underline py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">${listReportText[3]}</a>
                   </li>
                   <li>
-                    <a href="${listReportLink[4]}" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">${listReportText[4]}</a>
+                    <a href="${listReportLink[4]}" class="block px-4 hover:no-underline py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">${listReportText[4]}</a>
                   </li>
                   <li>
-                    <a href="${listReportLink[5]}" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">${listReportText[5]}</a>
+                    <a href="${listReportLink[5]}" class="block px-4 hover:no-underline py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">${listReportText[5]}</a>
                   </li>
                 </ul>
             </div>
         </li>
 
+        <!-- Others-->
         <li>
-            <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" class="flex items-center justify-between w-full py-2 px-3 text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:hover:bg-gray-700 md:dark:hover:bg-transparent">Dropdown <svg class="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
-        </svg></button>
+          <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" class="flex items-center justify-between w-full py-2 px-3 text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:hover:bg-gray-700 md:dark:hover:bg-transparent">
+            ${itemNavText[5]}
+            <svg class="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
+            </svg>
+          </button>
             <!-- Dropdown menu -->
             <div id="dropdownNavbar" class="z-10 absolute hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
                 <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownLargeButton">
-                  <li>
-                    <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
-                  </li>
-                  <li aria-labelledby="dropdownNavbarLink">
-                    <button id="doubleDropdownButton" data-dropdown-toggle="doubleDropdown" data-dropdown-placement="right-start" type="button" class="flex items-center justify-between w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dropdown<svg class="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
-  </svg></button>
-                    <div id="doubleDropdown" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
-                        <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="doubleDropdownButton">
-                          <li>
-                            <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Overview</a>
-                          </li>
-                          <li>
-                            <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">My downloads</a>
-                          </li>
-                          <li>
-                            <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Billing</a>
-                          </li>
-                          <li>
-                            <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Rewards</a>
-                          </li>
-                        </ul>
-                    </div>
-                  </li>
-                  <li>
-                    <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
-                  </li>
+                  
                 </ul>
-                <div class="py-1">
-                  <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign out</a>
-                </div>
             </div>
-        </li>
-        <li>
-          <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Services</a>
-        </li>
-        <li>
-          <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Pricing</a>
-        </li>
-        <li>
-          <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
         </li>
       </ul>
     </div>
--- front_end/Header&Navbar/navbar.js
+++ front_end/Header&Navbar/navbar.js
@@ -4,3 +4,17 @@ dropdown.forEach(element => {
         element.nextElementSibling.classList.toggle('hidden');
     })
 });
+
+var subdropdown = document.querySelectorAll('#dropdownNavbar');
+
+document.body.addEventListener('click', function(e) {
+    if(e.target.id != 'dropdownNavbarLink'){
+        subdropdown.forEach(element => {
+            if(!element.classList.contains('hidden')){
+                element.classList.add('hidden');
+            }
+        });
+    }
+})
+
+
